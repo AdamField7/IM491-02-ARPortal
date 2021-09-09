6bf883605b60c5faf0d334ad8cf0eb3e32cc8310
AdamField7 <89855015+AdamField7@users.noreply.github.com>
2021-09-09 18:51:29 -0500

    added ray

---
+++ Assets/Scripts/ARRaycast.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARRaycast : MonoBehaviour
+{
+    public Camera ARCamera;
+    public GameObject objectRef;
+    public List<GameObject> Planes;
+    public ARPlaneManager ARPM;
+    public static ARRaycast ARRC;
+
+
+    public void Start()
+    {
+        ARRC = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 )
+        {
+            Touch touch = Input.GetTouch(0);
+            
+            if(touch.phase == TouchPhase.Began)
+            {
+                Ray ray = ARCamera.ScreenPointToRay(touch.position);
+                RaycastHit hitObject;
+                if (Physics.Raycast(ray, out hitObject))
+                {
+                    objectRef.transform.position = hitObject.point;
+                }
+            }
+        }
+    }
+}
